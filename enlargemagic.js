85eabbbabf10a4ae77ffe991d660f0abbcfbaad1
MaoXie <42198201+GameAttacker@users.noreply.github.com>
2023-08-13 15:42:22 +0800

    Update enlargemagic.js

--- enlargemagic.js
+++ enlargemagic.js
@@ -15,7 +15,7 @@ TG频道群：https://t.me/py996
 *******************************
 [rewrite_local]
 
-^http[s]?:\/\/voicechat.enlargemagic.com url script-response-body https://raw.githubusercontent.com/89996462/Quantumult-X/main/ycdz/enlargemagic.js
+^http[s]?:\/\/voicechat.enlargemagic.com url script-response-body https://gameattacker.github.io/enlargemagic.js
 
 [mitm] 
 
